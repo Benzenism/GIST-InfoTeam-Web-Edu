4d8d247a9850101370a2fabb19b2b80ac2156c54
Benzenism <kimdk2964@gmail.com>
2025-03-22 15:39:40 +0900

    Update Sidebar

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").classList.add("sidebar-open");
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").classList.remove("sidebar-open");
-}
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").classList.add("show");
+    document.getElementById("main").classList.add("move");
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidebar").classList.remove("show");
+    document.getElementById("main").classList.remove("move");
+  }
+  
